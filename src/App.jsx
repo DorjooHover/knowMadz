d6328011bcce8737af2331099d8fd03e873c8a7a
DorjooHover <90739633+DorjooHover@users.noreply.github.com>
2022-04-05 15:44:36 +0800

    Loader

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Nav';
 import Header from './components/Header';
 import Experience from './components/Experience';
@@ -8,18 +8,44 @@ import Team from './components/Team';
 import Involve from './components/Involve';
 import Footer from './components/Footer';
 
+// Loader
+import PropagateLoader from 'react-spinners/PropagateLoader';
+
 const App = () => {
+	const [loading, setLoading] = useState(false);
+	let [color, setColor] = useState(
+		'linear-gradient(45deg, #f09433 0%,#e6683c 25%,#dc2743 50%,#cc2366 75%,#bc1888 100%)'
+	);
+
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+			setLoading(false);
+		}, 7000);
+	}, []);
 	return (
-		<>
-			<Navbar />
-			<Header />
-			<Experience />
-			<Banner />
-			<Event />
-			<Team />
-			<Involve />
-			<Footer />
-		</>
+		<div className="App">
+			{loading ? (
+				<div className="loader">
+					<PropagateLoader
+						size={10}
+						color={color}
+						loading={loading}
+					/>
+				</div>
+			) : (
+				<>
+					<Navbar />
+					<Header />
+					<Experience />
+					<Banner />
+					<Event />
+					<Team />
+					<Involve />
+					<Footer />
+				</>
+			)}
+		</div>
 	);
 };
 
