bdc83c080d578cc804c2394251f91ed508c18422
DorjooHover <90739633+DorjooHover@users.noreply.github.com>
2022-04-05 14:09:31 +0800

    Add files via upload

--- src/components/Involve/Input.jsx
+++ src/components/Involve/Input.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
 
+// const scriptURL =
+// 	'https://script.google.com/macros/s/AKfycbxkgWotCMyvIKoT9aGtPCndZdHtHrq8i972t9CKAVjO6xC7T0I/exec';
+// const form = document.forms['google-sheet'];
+
+// form.addEventListener('submit', e => {
+// 	e.preventDefault();
+// 	fetch(scriptURL, { method: 'POST', body: new FormData(form) })
+// 		.then(response => alert('Амжилттай бүртгэгдлээ..'))
+// 		.catch(error => console.error('Error!', error.message));
+// });
+
 const Input = () => {
 	return (
 		<form method="post" autocomplete="off" name="google-sheet">
