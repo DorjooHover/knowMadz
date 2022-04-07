01b7c09cae87a31d41f175b7e9c6473b622b71c5
DorjooHover <90739633+DorjooHover@users.noreply.github.com>
2022-04-08 02:57:31 +0800

    emailjs

--- src/components/Involve/Input.jsx
+++ src/components/Involve/Input.jsx
@@ -1,37 +1,71 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
-// const scriptURL =
-// 	'https://script.google.com/macros/s/AKfycbxkgWotCMyvIKoT9aGtPCndZdHtHrq8i972t9CKAVjO6xC7T0I/exec';
-// const form = document.forms['google-sheet'];
-
-// form.addEventListener('submit', e => {
-// 	e.preventDefault();
-// 	fetch(scriptURL, { method: 'POST', body: new FormData(form) })
-// 		.then(response => alert('Амжилттай бүртгэгдлээ..'))
-// 		.catch(error => console.error('Error!', error.message));
-// });
-
+// const Input = () => {
+// 	return (
+// 		<form method="post" autocomplete="off" name="google-sheet">
+// 			<div class="form-group">
+// 				<input
+// 					type="submit"
+// 					name="submit"
+// 					className="btn"
+// 					value="БҮРТГҮҮЛЭХ"
+// 				/>
+// 			</div>
+// 			<div class="form-group">
+// 				<input
+// 					type="text"
+// 					name="Email"
+// 					className="input__field"
+// 					placeholder="Имэйл хаяг...."
+// 					required=""
+// 				/>
+// 			</div>
+// 		</form>
+// 	);
+// };
 const Input = () => {
+	const form = useRef();
+
+	const sendEmail = e => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(
+				'service_h48gwis',
+				'template_tqw6iir',
+				form.current,
+				'q5enYcP6M3OFd9ddW'
+			)
+			.then(
+				result => {
+					console.log(result.text);
+				},
+				error => {
+					console.log(error.text);
+				}
+			);
+		e.target.reset();
+	};
+
 	return (
-		<form method="post" autocomplete="off" name="google-sheet">
-			<div class="form-group">
-				<input
-					type="submit"
-					name="submit"
-					className="btn"
-					value="БҮРТГҮҮЛЭХ"
-				/>
-			</div>
-			<div class="form-group">
-				<input
-					type="text"
-					name="Email"
-					className="input__field"
-					placeholder="Имэйл хаяг...."
-					value=""
-					required=""
-				/>
-			</div>
+		<form ref={form} onSubmit={sendEmail}>
+			{/* Button */}
+			<input
+				type="submit"
+				placeholder="БҮРТГҮҮЛЭХ"
+				value="БҮРТГҮҮЛЭХ"
+				className="btn"
+			/>
+
+			{/* Input Email */}
+			<input
+				type="email"
+				name="user_email"
+				className="input__field"
+				placeholder="Имэйл хаяг..."
+				name="email"
+			/>
 		</form>
 	);
 };
